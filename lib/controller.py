7ec41bff75d24b3090de7145cf0348851694d4ca
Markin.N.V <markin.agentf1@yandex.ru>
2023-08-23 13:03:25 +0300

    Рефактор к ветке 1

--- lib/controller.py
+++ lib/controller.py
@@ -8,7 +8,7 @@ from lib.base_classes.singleton import SingletonMetaClass
 
 class Controller(metaclass=SingletonMetaClass):
     _recognizer_config = namedtuple('Recognizer', ['language', 'model_path', 'online'])
-    _assistant_config = namedtuple('Assistant', ['name', 'sex', 'speech_language'])
+    _speaker_config = namedtuple('Assistant', ['name', 'sex', 'speech_language'])
 
     def __init__(self):
         try:
@@ -16,15 +16,8 @@ class Controller(metaclass=SingletonMetaClass):
                 CONFIG = load(config_file, SafeLoader)
                 self.version: str = CONFIG['version']
                 self.recognizer_config: Controller._recognizer_config = self._recognizer_config(**CONFIG['recognizer'])
-                self.assistant_config: Controller._assistant_config = self._assistant_config(**CONFIG['assistant'])
+                self.speaker_config: Controller._speaker_config = self._speaker_config(**CONFIG['speaker'])
 
         except Exception as exp:
             logger.error(f"Произошла ошибка при считывании конфига. Прекращение работы. \n{exp}")
             exit(1)
-
-
-if __name__ == "__main__":
-    s = Controller()
-    print(s)
-    s1 = Controller()
-    print(s1)
--- lib/logger.py
+++ lib/logger.py
@@ -1,6 +1,6 @@
 import logging
 
-logging.basicConfig(handlers=(logging.FileHandler('output.log'), logging.StreamHandler()),
+logging.basicConfig(handlers=(logging.FileHandler(f'{__name__}.log'), logging.StreamHandler()),
                     format='[%(asctime)s] [%(levelname)s] %(message)s',
                     datefmt='%d-%m-%Y %H:%M:%S',
                     level=logging.DEBUG)
